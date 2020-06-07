4102e0c35852331db98571a5a6bdd795760a8352
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-06-07 12:02:35 +0100

    server: new route to create points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,14 +9,56 @@ routes.get('/', (request, response) => {
 
 routes.get('/items', async (request, response) => {
   const items = await knex('items').select('*');
+
   const serializedItems = items.map((item) => {
     return {
+      'id': item.id,
       'title': item.title,
       'image_url': `http://localhost:3333/uploads/${item.image}`,
     }
   });
 
   return response.json(serializedItems);
+});
+
+routes.post('/points', async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+   } = request.body;
+
+   const trx = await knex.transaction();
+
+   const insertedIds = await trx('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+   });
+
+   const point_id = insertedIds[0];
+
+   const pointItems = items.map((item_id: number) => {
+     return {
+      item_id,
+      point_id,
+     }
+   })
+
+   await trx('points_items').insert(pointItems);
+
+   // without error handle
+   response.status(201).send();
 });
 
 export default routes;
